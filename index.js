82c215f539472560c1abb6721c51476940dc737e
Hain Thit <hainlthit@gmail.com>
2021-12-15 11:43:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record){
+    const findWin = record.find(function(season){
+        return season.result === 'W'
+    })
+    if (findWin){
+    return findWin.year
+    }
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -4,7 +4,7 @@ describe('superbowlWin(record)', function () {
     { year: "2018", result: "N/A"},
     { year: "2017", result: "N/A"},
     { year: "2016", result: "N/A"},
-    { year: "2015", result: "W"},
+    { year: "1976", result: "W"},
     { year: "2014", result: "N/A"},
     { year: "2013", result: "L"},
     { year: "2012", result: "N/A"},
